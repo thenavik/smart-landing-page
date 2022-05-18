ac429127a7b764e5bb9422b680c0b8171bfd7078
jovid1242 <jovid1242@gmail.com>
2022-05-18 18:16:07 +0500

    add feature

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import About from "../components/about/About";
+import Feature from "../components/feature/Feature";
 
 export default function Home() {
   return (
     <div>
       <h2>dcdvsvd</h2>
       <About />
+      <Feature />
     </div>
   );
 }
