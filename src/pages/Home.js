f06406cbabb7c3277df02b5e875e30ba6fd7e018
thenavik <iamdigitalist@gmail.com>
2022-05-18 16:11:05 +0300

    Header (navigation bar)

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,11 @@
 import React from "react";
 import About from "../components/about/About";
+import Header from "../components/about/Header";
 
 export default function Home() {
   return (
     <div>
-      <h2>dcdvsvd</h2>
+      <Header />
       <About />
     </div>
   );
