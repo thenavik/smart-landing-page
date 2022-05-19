9f90cdefa850f1448fb7ee321ba5089bb29300f2
thenavik <iamdigitalist@gmail.com>
2022-05-19 14:04:29 +0300

    have done footer

--- src/core/XButton.js
+++ src/core/XButton.js
@@ -1,13 +1,12 @@
 import React from "react";
-import { BsCart4 } from "react-icons/bs";
 import "../styles/core/xbutton.scss";
 
 export default function XButton(props) {
   return (
     <>
       <button className={props.type}>
-        <BsCart4 />
-        {props.name}
+        {props?.icon}
+        {props?.name}
       </button>
     </>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,8 +7,13 @@ import CoreFeature from "../components/feature/CoreFeature";
 import WhyChoose from "../components/whychoose/WhyChoose";
 import Slider from "../components/slider/Slider";
 import Newsletter from "../components/newsletter/Newsletter";
+import Faq from "../components/faq/Faq";
 import ProductArea from "../components/product/ProductArea";
+<<<<<<< HEAD
 import Footer from "../components/footer/Footer";
+=======
+import Contact from "../components/contact/Contact";
+>>>>>>> 64a3c5de86a5fcf565cd3030fc2764e9ac4b3683
 
 export default function Home() {
   return (
@@ -22,7 +27,12 @@ export default function Home() {
       <Newsletter />
       <ProductArea />
       <Slider />
+<<<<<<< HEAD
       <Footer />
+=======
+      <Faq />
+      <Contact />
+>>>>>>> 64a3c5de86a5fcf565cd3030fc2764e9ac4b3683
     </div>
   );
 }
